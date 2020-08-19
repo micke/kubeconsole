dcaaf81c15d0256a8527454344522ea513913f9b
Micke Lisinge <hi@micke.me>
2020-08-19 16:49:37 +0200

    Build completion in buildtime

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 set -e
 
-mkdir -p build/mac
+mkdir -p build/mac/completion
 
 go build  -ldflags="-w -s" -o build/mac/kubeconsole main.go
 cd build/mac
-tar -czf ../mac.tar.gz kubeconsole
+./kubeconsole completion bash > completion/bash
+./kubeconsole completion zsh > completion/zsh
+tar -czf ../mac.tar.gz kubeconsole completion
 cd ../..
 shasum -a 256 build/mac.tar.gz
