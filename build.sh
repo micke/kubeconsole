d17e031d3c73a3dd3cc46997d79a242e6a91fbd9
Micke Lisinge <hi@micke.me>
2020-08-19 15:41:01 +0200

    Fix binary name

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ set -e
 
 mkdir -p build/mac
 
-go build  -ldflags="-w -s" -o build/mac/devflow main.go
+go build  -ldflags="-w -s" -o build/mac/kubeconsole main.go
 cd build/mac
-tar -czf ../mac.tar.gz devflow
+tar -czf ../mac.tar.gz kubeconsole
 cd ../..
 shasum -a 256 build/mac.tar.gz
