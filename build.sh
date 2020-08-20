cf1ba2f1ae7416a91eb331deb1e8b9f011a2d54e
Hannes Hornwall <hannes@hornwall.me>
2020-08-20 16:12:25 +0200

    Add MacOS/Linux cross compile to build script

--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
 set -e
 
 mkdir -p build/mac/completion
+mkdir -p build/linux/completion
 
-go build  -ldflags="-w -s" -o build/mac/kubeconsole main.go
+env GOOS=darwin go build -ldflags="-w -s" -o build/mac/kubeconsole main.go
+env GOOS=linux go build -ldflags="-w -s" -o build/linux/kubeconsole main.go
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  ./build/linux/kubeconsole completion bash | tee ./build/mac/completion/bash ./build/linux/completion/bash
+  ./build/linux/kubeconsole completion zsh | tee ./build/mac/completion/zsh ./build/linux/completion/zsh
+elif [["$OSTYPE" == "darwin"* ]]; then
+  ./build/mac/kubeconsole completion bash | tee ./build/mac/completion/bash ./build/linux/completion/bash
+  ./build/mac/kubeconsole completion zsh | tee ./build/mac/completion/zsh ./build/linux/completion/zsh
+fi
+
 cd build/mac
-./kubeconsole completion bash > completion/bash
-./kubeconsole completion zsh > completion/zsh
 tar -czf ../mac.tar.gz kubeconsole completion
-cd ../..
+cd -
+cd build/linux
+tar -czf ../linux.tar.gz kubeconsole completion
+cd -
+
+echo mac SHA
 shasum -a 256 build/mac.tar.gz
+echo linux SHA
+shasum -a 256 build/linux.tar.gz
