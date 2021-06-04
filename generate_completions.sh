261da196e85ea5bd825c4dd028fd51c6a79d0586
Jacob Carlborg <jacob.carlborg@apoex.se>
2021-06-04 14:06:47 +0200

    Add support for fish shell completions

--- generate_completions.sh
+++ generate_completions.sh
@@ -5,6 +5,6 @@ set -e
 rm -rf completions
 mkdir -p completions
 
-for shell in bash zsh; do
+for shell in bash zsh fish; do
   go run main.go completion "$shell" > "completions/kubeconsole.$shell"
 done
